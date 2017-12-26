0d7bbfcff1470696092664c215d9681f99fef1f1
palmer <yepm1992@163.com>
2017-12-26 22:08:43 +0800

    zip

--- zip.js
+++ zip.js
@@ -2,10 +2,33 @@
  * @Author: palmerye 
  * @Date: 2017-12-26 21:29:17 
  * @Last Modified by: palmerye
- * @Last Modified time: 2017-12-26 21:32:09
+ * @Last Modified time: 2017-12-26 22:05:28
  */
 
- var fs = require('fs')
- var zlib = require('zlib')
+var fs = require('fs')
+var zlib = require('zlib')
 
- 
+console.log('开始压缩 >>>>>>')
+fs.rename(
+    './example/dir_1/dir_1.js',
+    './example/dir_2/dir_1.js',
+    (err) => {
+        if (err) {
+            throw err
+        }
+        console.log('done...')
+    }
+)
+fs.rename(
+    './example/dir_1/',
+    './example/dir_2/dir_1/',
+    (err) => {
+        if (err) {
+            throw err
+        }
+        console.log('done...')
+        console.log('压缩成功 >>>>>>')
+    }
+)
+zlib.createGzip()
+fs.createWriteStream('./example/dir_2.gz')
